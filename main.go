9f7af426496b8a9f2a20e8ddf85fe2ec58a88404
Ryan Yogan <ryanyogan@gmail.com>
2018-03-23 18:33:24 -0700

    Specify the port via env var

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ryanyogan/k8-heart-beat/handlers"
 )
 
+// PORT=8000[specify your own port number] go run main.go
 func main() {
 	log.Print("Starting heart beat service...")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port was not set in environment variables.")
+	}
+
 	router := handlers.Router()
 	log.Print("The service is ready to listen and serve requests...")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
